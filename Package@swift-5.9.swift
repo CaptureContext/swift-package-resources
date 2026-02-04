476def6e007e1d7f1b029ee6619bb859c9bd78fe
maximkrouk <id.maximkrouk@gmail.com>
2026-02-04 07:15:40 +0100

    feat(Swift-6): Use Swift 6 as primary swift version

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -29,6 +29,5 @@ let package = Package(
 			]
 		),
 		.target(name: "PackageResourcesCore"),
-	],
-	swiftLanguageModes: [.v6]
+	]
 )
